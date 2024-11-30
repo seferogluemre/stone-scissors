c8b06b80611fd91f62837c9e6123b7bc7144e833
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-30 23:38:41 +0300

    second commit

--- StoneScis.py
+++ StoneScis.py
@@ -6,4 +6,14 @@ def stone_scissors():
     options=["Taş","Kagıt","Makas"]
 
     while True:
-        user_prompt=input("Seçiminizi yapın")
+        user_prompt=input("Seçiminizi yapınız (Taş,Kagıt,Makas)").capitalize()
+
+        if user_prompt=="çıkış" || user_prompt=="Çıkış" :
+            print("Oyun Sona erdi")
+
+        if user_prompt not in options:
+            print("Geçersiz Seçim. Tekrar Deneyin")
+            continue
+
+        comp_selected = random.choice(options)
+        print(f"Bilgisayarın seçimi: {comp_selected}")
